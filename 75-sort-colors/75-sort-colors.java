1e104b1b927f481f805f69c4b82e01668a29aff7
Pranay Patil <74552410+Pranay39@users.noreply.github.com>
2022-09-29 01:21:26 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (45.96%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,19 +1,37 @@
 class Solution {
     public void sortColors(int[] nums) {
-         for(int i = 0; i < nums.length - 1; i++){
-            for(int j = i + 1; j < nums.length; j++){
-                if(nums[j] < nums[i]){
+//          for(int i = 0; i < nums.length - 1; i++){
+//             for(int j = i + 1; j < nums.length; j++){
+//                 if(nums[j] < nums[i]){
                     
-                    swap(nums, i, j);
-                }
-            }
+//                     swap(nums, i, j);
+//                 }
+//             }
+//         }
+//     }  
+    
+//     static void swap(int[]nums, int i, int j){
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+        
+        int count0 = 0, count1= 0, count2 = 0; 
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 0)
+                count0++;
+            else if(nums[i] == 1)
+                count1++;
+            else
+                count2++;
         }
-    }  
+        
+        for(int i = 0; i < count0; i++)
+            nums[i] = 0;
+        for(int i = 0; i < count1; i++)
+            nums[count0 + i] = 1;
+        for(int i = 0; i < count2; i++)
+            nums[count0 + count1 + i] = 2;
     
-    static void swap(int[]nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
+}
 }
